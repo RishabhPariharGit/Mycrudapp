28d2bfb07420635d11ff8edbc31c26678c8917d0
sachin <rishabhgit1704@gmail.comin>
2024-05-29 10:44:04 +0530

    added the files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react"
+import axios from "axios"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+ 
+const [playerdt, setplayerdt] = useState(null);
+
+useEffect(() => {
+  fetchDet();
+}, [])
+
+
+const fetchDet = async() =>{
+
+const res = await axios.get("http://localhost:5070/plydt");
+  
+setplayerdt(res.data.getplayercompt)
+
+
 }
 
-export default App;
+//   return(<div className="App">
+// <table>
+//             <tr>
+//     <th>Person 1</th>
+//     <th>Person 2</th>
+//     <th>Person 3</th>
+//   </tr>
+//       {playerdt && playerdt.map((det) => {
+//         return(
+//           <div>
+            
+//               <tr>
+//                 <td>{det.name}</td>
+//                 <td>{det.club}</td>
+//                 <td>{det.goals}</td>
+//               </tr>
+//           </div>
+//         )
+//       })}
+//       </table>
+//     </div>
+//   );
+
+
+return (
+  <div className="App">
+      <table>
+          <tr>
+              <th>Name</th>
+              <th>Club</th>
+              <th>goals</th>
+          </tr>
+          {playerdt && playerdt.map((det) => {
+              return (
+                  <tr>
+                      <td>{det.name}</td>
+                      <td>{det.club}</td>
+                      <td>{det.goals}</td>
+                  </tr>
+              )
+          })}
+      </table>
+  </div>
+);
+
+}
+
+export default App
